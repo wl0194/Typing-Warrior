e6f93b93164142a6812a13a30f2e8eb3bf946747
Amy Hwang <728amyh@gmail.com>
2021-12-09 14:10:08 -0500

    created env

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require("sequelize/dist");
+
+// set up User table
+const User = sequalize.define('users', {
+    id: {
+        type: Sequelize.INTERGER,
+        
+    }
+})
+    
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
-
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
