bf17cb205e6cfef9280680743d15c484375e53aa
Amy Hwang <728amyh@gmail.com>
2021-12-09 14:35:57 -0500

    created user model

--- models/user.js
+++ models/user.js
@@ -4,7 +4,24 @@ const { Sequelize } = require("sequelize/dist");
 const User = sequalize.define('users', {
     id: {
         type: Sequelize.INTERGER,
-        
+        unique: true,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: Sequalize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
-})
+});
+
+User.beforeCreate((user, options) => {
+    const salt = bcryt.genSaltSync();
+    user.password = bcryt.hashSync(user.password, salt);
+});
     
