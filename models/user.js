4d2d3e46a7a0f9ecfd984ecb8e9bd73e9b7cd71d
Amy Hwang <728amyh@gmail.com>
2021-12-11 14:07:14 -0500

    added login

--- models/user.js
+++ models/user.js
@@ -1,30 +1,49 @@
-const { Sequelize } = require("sequelize/dist");
-const bcrypt = require('bcrypt');
+const {Model, DataTypes}= require('sequelize');
+// const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-// set up User table
-const User = sequalize.define('users', {
-    id: {
-        type: Sequelize.INTERGER,
-        unique: true,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    username: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
     }
-});
+  }
 
-User.beforeCreate((user, options) => {
-    // This doesn't allow a user to crack/calculate other users passwords
-    const salt = bcrypt.genSaltSync();
-    user.password = bcryt.hashSync(user.password, salt);
-});
+  User.init(
+      {
+          id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isEmail: true
+                }
 
-module.exports = User;
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len:[8]
+                },
+            },
+      },
+      {
+          sequelize,
+          timestamps: true,
+          freezeTableName: true,
+          underscored: true,
+          modelName: 'user'
+      }
+  );
+
+  module.exports = User;
