4f9680fd91ec99004a060310b42edcddb7a814ae
Gus <gus@Guss-MBP.fios-router.home>
2021-12-15 18:39:21 -0500

    Add changes to routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,8 +2,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-<<<<<<< HEAD
-=======
   
 router.post('/', async (req, res) => {
   try {
@@ -20,7 +18,6 @@ router.post('/', async (req, res) => {
   }
 });
 
->>>>>>> 7921b5690c44adedb5a0b5c30202a6e0b10e40ed
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const session = require('express-session');
+const session = require('express-session');
 const exphbs = require("express-handlebars");
 const path = require('path');
 const app = express();
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
 
 const sequelize = require("./config/connection.js");
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
