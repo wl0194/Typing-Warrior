7da5ede1bcb74e3057b761688d9175c5c1701e40
Gus <gus@Guss-MBP.fios-router.home>
2021-12-17 20:22:58 -0500

    updates

--- public/js/login.js
+++ public/js/login.js
@@ -24,30 +24,6 @@ const loginFormHandler = async (event) => {
   }
 };
 
-<<<<<<< HEAD
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  // const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-=======
->>>>>>> e5e2b5940b5c11b2d3f429c5bbd1a5df64ce95f9
 
 document
   .querySelector('#login-form')
